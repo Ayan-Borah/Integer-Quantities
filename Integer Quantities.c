11686fc9bdb1aeed0beaafc5771d4e4fa2b02c23
Ayan Borah <ayanborah30@gmail.com>
2024-09-25 23:57:07 +0530

    Add files via upload

---
+++ Integer Quantities.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+float root(int, int);
+main()
+{
+	int x=10, y=25;
+	float f_num1, f_num2;
+	f_num1=(float)x;
+	f_num2=(float)y;
+	printf("intger: %d  %d,\n floating point: %.1f  %.1f \n", x, y, f_num1, f_num2);
+	return 0;
+}
